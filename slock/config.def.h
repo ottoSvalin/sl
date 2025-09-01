618bb52b1faf85bdf3bcc5e04027d83456f5f42d
Otto Svalin <04otto.svalin@gmail.com>
2025-09-01 16:48:10 +0200

    dmenu center+border

--- slock/config.def.h
+++ slock/config.def.h
@@ -14,3 +14,12 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = ":^)";
+
+/* text color */
+static const char * text_color = "#777777";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x10";
